2f1fcc96a2f87a7e4fdb94242bd1cc9da202f725
Gregg Reynolds <dev@mobileink.com>
2013-03-31 10:32:24 -0500

    implement plugin, remove code it replaces

---
+++ plugin/project.clj
@@ -0,0 +1,8 @@
+(defproject gaem "0.3.0-SNAPSHOT"
+  :description "Leiningen plugin for appengine-magic sdk"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/tools.logging "0.2.3"]
+                 [com.google.appengine/appengine-tools-sdk "1.7.6"]]
+  :eval-in-leiningen true)
--- src/leiningen/appengine_update.clj
+++
@@ -1,7 +0,0 @@
-(ns leiningen.appengine-update
-  "Deploys the application to the production Google App Engine."
-  (:use appengine-magic.leiningen-helpers))
-
-
-(defn appengine-update [project app-name & [app-version]]
-  (run-with-appengine-app-versions "appengine-update" project app-name app-version))
